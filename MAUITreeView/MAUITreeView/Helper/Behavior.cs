92f531c4ea4d6e334c2a0d7904f7923867a04fa2
JayashreeSF3546 <jayashree.ravishankar@syncfusion.com>
2025-06-16 15:39:44 +0530

    Added sample for how-to-customize-the-drag-item-view

---
+++ MAUITreeView/MAUITreeView/Helper/Behavior.cs
@@ -0,0 +1,50 @@
+﻿using Syncfusion.Maui.Core.Carousel;
+using Syncfusion.Maui.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUITreeView
+{
+    public class Behavior : Behavior<SfTreeView>
+    {
+        private FileManagerViewModel viewModel;
+        private SfTreeView treeView;
+        protected override void OnAttachedTo(SfTreeView bindable)
+        {
+            base.OnAttachedTo(bindable);
+            treeView = bindable;
+            bindable.Loaded += OnTreeViewLoaded;            
+            bindable.ItemDragging += OnTreeViewDragging;
+        }
+
+        private void OnTreeViewLoaded(object? sender, TreeViewLoadedEventArgs e)
+        {
+            viewModel = treeView.BindingContext as FileManagerViewModel;
+        }
+
+        private void OnTreeViewDragging(object? sender, ItemDraggingEventArgs e)
+        {
+            if (e.Action == Syncfusion.Maui.TreeView.DragAction.Dragging)
+            {
+                if (e.DropPosition == Syncfusion.Maui.TreeView.DropPosition.None)
+                {
+                    viewModel.BlockIconVisible = true;
+                }
+                else
+                {
+                    viewModel.BlockIconVisible = false;
+                }
+            }
+        }
+
+        protected override void OnDetachingFrom(SfTreeView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.Loaded -= OnTreeViewLoaded;
+            bindable.ItemDragging -= OnTreeViewDragging;
+        }
+    }
+}
