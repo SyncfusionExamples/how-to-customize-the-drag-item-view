a8a4043b8d471e2040ffdbf84cf67d87d9fe840e
JayashreeSF3546 <jayashree.ravishankar@syncfusion.com>
2025-06-18 15:59:25 +0530

    review changes

--- MAUITreeView/MAUITreeView/MauiProgram.cs
+++ MAUITreeView/MAUITreeView/MauiProgram.cs
@@ -15,7 +15,6 @@ namespace MAUITreeView
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("MauiMaterialAssets.ttf", "MauiMaterialAssets");
                 });
 
 #if DEBUG
